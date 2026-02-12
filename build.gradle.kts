b5c7b4185df84d6d942ef9d1d2be1d230caa4c45
Ran Aroussi <ran@aroussi.com>
2026-02-12 10:28:50 +0000

    fix: upgrade Kotlin to 2.2.21 to match okhttp 5.x requirements

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.serialization") version "2.0.21"
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.serialization") version "2.2.21"
     id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
